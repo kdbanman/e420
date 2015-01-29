5a47e42ad1587f087fa54e9f2a59647293e5249d
kdbanman <kirby.banman@gmail.com>
2015-01-29 14:03:43 -0700

    finalize report.  clean make and bash scripts.

--- lab1/experiment.sh
+++ lab1/experiment.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# Kirby Banman
+# ECE 420 Lab 1
+# 15 Jan 2015
+#
+# File:  
+#    experiment.sh
+#
+# Purpose:
+#    gather timing test data for experimental analysis
+#    
+# Usage:
+#    AFTER `make matr` or `make run`:
+#
+#    bash experiment.sh
+
 sizes=( 60 120 180 240 300 480 960 )
 th_counts=( 1 4 9 16 25)
 
