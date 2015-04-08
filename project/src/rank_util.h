f454e6b5e4bf98747f308c335fd20c209bf3f724
kdbanman <kirby.banman@gmail.com>
2015-04-07 20:45:40 -0600

    add rank_utility hole.

---
+++ project/src/rank_util.h
@@ -0,0 +1,22 @@
+/*
+ * Kirby Banman
+ * ECE 420 Project
+ * 10 April 2015
+ *
+ * File:
+ *    rank_util.c
+ *
+ * Purpose:
+ *    Pagerank utility methods
+ *
+ * Compile:
+ *    gcc -Wall -Wextra -o bin/rank_util.o -c src/rank_util.c
+ *
+ */
+
+#include "graph.h"
+
+#ifndef _RANK_UTIL_H_
+#define _RANK_UTIL_H_
+
+#endif
